db03a49cd59f6c603dadc71b8eaae2553e6b3e69
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2024-02-11 08:37:05 +0530

    Update startup.py

--- TelethonHell/utils/startup.py
+++ TelethonHell/utils/startup.py
@@ -98,8 +98,8 @@ async def start_msg(client, pic, version, total):
 async def join_it(client):
     if client:
         try:
-            await client(functions.channels.JoinChannelRequest("ll_THE_BAD_BOT_ll"))
-            await client(functions.messages.ImportChatInviteRequest("II_CHAT_HUB_II"))
+            await client(functions.channels.JoinChannelRequest("@ll_THE_BAD_BOT_ll"))
+            await client(functions.messages.ImportChatInviteRequest("@ll_BAD_MUNDA_WORLD_ll"))
         except BaseException:
             pass
 
